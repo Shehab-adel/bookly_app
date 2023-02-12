221bcb05ee38306fad47c24209ed05f9187da921
Shehab-adel <shoopa5050@gmail.com>
2023-02-12 15:15:22 +0200

    init the project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/view/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}

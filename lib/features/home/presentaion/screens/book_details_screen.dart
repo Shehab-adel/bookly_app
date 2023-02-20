1dca803542fd1d855224248a5eca29d25ae8e960
Shehab-adel <shoopa5050@gmail.com>
2023-02-20 14:01:08 +0200

    added image to details screen

--- lib/features/home/presentaion/screens/book_details_screen.dart
+++ lib/features/home/presentaion/screens/book_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentaion/screens/widgets/book_details_screen_appbare_widget.dart';
+import 'package:bookly_app/features/home/presentaion/screens/widgets/listview_item_widget.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsScreen extends StatelessWidget {
@@ -6,12 +7,18 @@ class BookDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30.0),
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
         child: Column(
           children: [
-            BookDetailsScreenAppbareWidget(),
+            const BookDetailsScreenAppbareWidget(),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: size.width * .16, vertical: size.height * .04),
+              child: const ListViewItemWidget(),
+            )
           ],
         ),
       ),
